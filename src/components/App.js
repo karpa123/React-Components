7188d99c66ea8e38bd91e579bc7b3d73436402ea
Alexey Zakharchuk <alexey.zak@architech.nyc>
2021-05-09 19:08:21 +0300

    add project folders

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import logo from '../static/images/logo.svg';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
